627d99f36155cf78ad7fa32e2990072b39cdb744
Dmitriy Tihonov <114222693+SomeClade@users.noreply.github.com>
2024-02-08 16:59:53 +0300

    Проверка дедушки

---
+++ tasks/task3Var5.pl
@@ -0,0 +1,61 @@
+man(voeneg).
+man(ratibor).
+man(boguslav).
+man(velerad).
+man(duhovlad).
+man(svyatoslav).
+man(dobrozhir).
+man(bogomil).
+man(zlatomir).
+
+woman(goluba).
+woman(lubomila).
+woman(bratislava).
+woman(veslava).
+woman(zhdana).
+woman(bozhedara).
+woman(broneslava).
+woman(veselina).
+woman(zdislava).
+
+parent(voeneg,ratibor).
+parent(voeneg,bratislava).
+parent(voeneg,velerad).
+parent(voeneg,zhdana).
+
+parent(goluba,ratibor).
+parent(goluba,bratislava).
+parent(goluba,velerad).
+parent(goluba,zhdana).
+
+parent(ratibor,svyatoslav).
+parent(ratibor,dobrozhir).
+parent(lubomila,svyatoslav).
+parent(lubomila,dobrozhir).
+
+parent(boguslav,bogomil).
+parent(boguslav,bozhedara).
+parent(bratislava,bogomil).
+parent(bratislava,bozhedara).
+
+parent(velerad,broneslava).
+parent(velerad,veselina).
+parent(veslava,broneslava).
+parent(veslava,veselina).
+
+parent(duhovlad,zdislava).
+parent(duhovlad,zlatomir).
+parent(zhdana,zdislava).
+parent(zhdana,zlatomir).
+
+
+men():- man(X), print(X), nl, fail.
+women():- woman(X), print(X), nl, fail.
+children(X):- parent(X,Y), print(Y), nl, fail.
+
+mother(X,Y):- woman(X), parent(X,Y).
+mother(X):- mother(Y,X), print(Y), nl, fail.
+
+% провер€ет, €вл€етс€ ли X дедушкой Y. ƒедушкой €вл€етс€ мужчина, который €вл€етс€ родителем родител€ Y.
+
+grand_pa(X, Y) :- parent(X, Z), parent(Z, Y),man(X).
