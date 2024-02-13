fc1d6fddadbcf722c032fe84b43073bd97947d00
Dmitriy Tihonov <114222693+SomeClade@users.noreply.github.com>
2024-02-13 11:01:08 +0300

    Добавлена рекурсия факториала вверх/вниз

--- tasks/lr2/numerical_operations.pl
+++ tasks/lr2/numerical_operations.pl
@@ -3,3 +3,21 @@
 max(X, Y, U, Z) :-
     TempMax is max(X, max(Y, U)),
     Z = TempMax.
+%Факториал рекурсия вниз +N,-X
+
+fact_down(0, 1).
+fact_down(N, X) :-
+    N > 0,
+    N1 is N - 1,
+    fact_down(N1, X1),
+    X is N * X1.
+
+%Рекурсия вверх (+N,-X)
+fact_up(N, X) :- fact_up_helper(N, 1, X).
+
+fact_up_helper(0, Acc, Acc).
+fact_up_helper(N, Acc, X) :-
+    N > 0,
+    N1 is N - 1,
+    Acc1 is N * Acc,
+    fact_up_helper(N1, Acc1, X).
