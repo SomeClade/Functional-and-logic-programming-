e43e3c4facf37de7b89cef41d06c155aaacef601
SomeClade <114222693+SomeClade@users.noreply.github.com>
2024-02-14 13:29:59 +0300

    Созданы предикаты поиска максимальной цифры в числе рекурсия вверх/вниз

---
+++ tasks/lr2/max_min_nod_task2.pl
@@ -0,0 +1,26 @@
+% max_digit_down(+Number, -MaxDigit)
+max_digit_down(Number, MaxDigit) :- max_digit_down(Number, 0, MaxDigit).
+
+% max_digit_down(+Number, +CurrentMax, -MaxDigit)
+% Вспомогательный предикат с аккумулятором( то есть для сохранения промежуточных результатов)
+max_digit_down(0, CurrentMax, CurrentMax) :- !.
+max_digit_down(Number, CurrentMax, MaxDigit) :-
+    Number > 0,
+    NextDigit is Number mod 10,
+    NextNumber is Number // 10,
+    NewMax is max(CurrentMax, NextDigit),
+    max_digit_down(NextNumber, NewMax, MaxDigit).
+
+
+% max_digit_up(+Number, -MaxDigit)
+max_digit_up(Number, MaxDigit) :- max_digit_up(Number, 0, MaxDigit).
+
+% max_digit_up(+Number, +Acc, -MaxDigit)
+% Acc - аккумулятор, хранит текущее максимальное значение.
+max_digit_up(0, Acc, Acc).
+max_digit_up(Number, Acc, MaxDigit) :-
+    Number > 0,
+    NextDigit is Number mod 10,
+    NewAcc is max(Acc, NextDigit),
+    NextNumber is Number // 10,
+    max_digit_up(NextNumber, NewAcc, MaxDigit).
