8d16554b2e606b049316e548d8314a94d1c3aba5
SomeClade <114222693+SomeClade@users.noreply.github.com>
2024-02-18 14:43:35 +0300

    Добавлены предикаты
    Добавлены предикаты чтения и вывода списка, добавлен предикат определяющий является ли число с заданным индексом глобальным минимумом

---
+++ tasks/lr2/glob-min_miss-num_elements_task3.pl
@@ -0,0 +1,25 @@
+% предикаты для чтения и вывода списка
+
+% Чтение списка с консоли
+read_list(List) :-
+    write('Enter list elements, end with "end".: '), nl,
+    read_line(List).
+
+read_line([X|List]) :-
+    read(X), X \= end,
+    !, read_line(List).
+read_line([]).
+
+% Вывод списка на экран
+write_answer(List) :-
+    write(List), nl.
+
+
+%является ли элемент по указанному индексу глобальным минимумом
+
+% is_global_minimum(+List, +Index, -Result)
+% List - входной список, Index - индекс элемента для проверки, Result - результат проверки (yes/no)
+is_global_minimum(List, Index, Result) :-
+    nth0(Index, List, Element),  % Получаем элемент по индексу
+    min_list(List, Min),  % Находим минимальный элемент в списке
+    (Element =:= Min -> Result = yes; Result = no).  % Сравниваем элемент с минимальным
