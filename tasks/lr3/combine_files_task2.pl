948a38f1273855bdde867b81532f309063aa2b6d
SomeClade <114222693+SomeClade@users.noreply.github.com>
2024-02-20 15:33:13 +0300

    Предикат вывода кол-ва строк без пробелов

--- tasks/lr3/combine_files_task2.pl
+++ tasks/lr3/combine_files_task2.pl
@@ -32,7 +32,23 @@ max_line_length(Lines, MaxLength) :-
     maplist(string_length, Lines, Lengths),
     max_list(Lengths, MaxLength).
 
+% Задача 2: Количество строк без пробелов
+lines_without_spaces(Lines, Count) :-
+    include(\+sub_string(_, _, _, _, " "), Lines, FilteredLines),
+    length(FilteredLines, Count).
+
+% Вспомогательные предикаты для вывода
+print_max_line_length(FileName) :-
+    read_lines_from_file(FileName, Lines),
+    max_line_length(Lines, MaxLength),
+    format('The longest line length is: ~w~n', [MaxLength]).
+
+print_lines_without_spaces(FileName) :-
+    read_lines_from_file(FileName, Lines),
+    lines_without_spaces(Lines, Count),
+    format('Lines without spaces: ~w~n', [Count]).
+
 
 % Пример вызова
-% :- print_max_line_length('your_file.txt').
-
+% :- print_max_line_length('file.txt').
+% :- print_lines_without_spaces('file.txt').
