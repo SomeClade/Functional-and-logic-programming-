c1db623bcbb6d4007238fc68c1653923cff2b09d
SomeClade <114222693+SomeClade@users.noreply.github.com>
2024-03-22 14:42:20 +0300

    ЛР3 Задача 6
    Создан предикат создания всех слов длины 8 из трёх букв

---
+++ tasks/lr3/three_in_word_task6.pl
@@ -0,0 +1,24 @@
+% Предикат для создания всех слов длины 8 из алфавита {a,b,c,d,e,f},состоящих ровно из трех различных букв.
+% generate_three_letter_words(+Alphabet, +FileName) 
+generate_three_letter_words(Alphabet, FileName) :-
+    open(FileName, write, Stream), 
+    findall(Word, (length(Word, 8),
+                   maplist(member(Alphabet), Word),  
+                   three_different_letters(Word)), 
+            Words),
+    maplist(write_word(Stream), Words),
+    close(Stream).
+
+% Предикат для проверки, что в слове ровно три различные буквы.
+%three_different_letters(+Word) 
+three_different_letters(Word) :-
+    sort(Word, UniqueLetters),
+    length(UniqueLetters, 3).
+
+% Предикат для записи слова в файл. write_word(+Stream, +Word)
+write_word(Stream, Word) :-
+    atom_chars(WordAtom, Word),  % Преобразуем список символов в атом
+    write(Stream, WordAtom),     % Пишем атом в файл
+    write(Stream, '\n').         % Новая строка после каждого слова
+
+% Пример вызова: generate_three_letter_words([a,b,c,d,e,f], 'output.txt').
